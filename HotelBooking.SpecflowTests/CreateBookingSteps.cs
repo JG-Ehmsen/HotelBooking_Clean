79e7c5afdc1eb0178623c2b862f0a40907b693c7
Kristoffer Christensen <kristoffermchristensen@gmail.com>
2019-11-02 12:15:58 +0100

    Specflow testing

---
+++ HotelBooking.SpecflowTests/CreateBookingSteps.cs
@@ -0,0 +1,106 @@
+﻿using HotelBooking.Core;
+using Moq;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace HotelBooking.SpecflowTests
+{
+    [Binding]
+    public class CreateBookingSteps
+    {
+        private IBookingManager bookingManager;
+
+        Mock<IRepository<Booking>> fakeBookingRepository;
+        Mock<IRepository<Room>> fakeRoomRepository;
+
+        private static DateTime d = DateTime.Today;
+        private static DateTime firstInMonth1 = new DateTime(d.Year, d.Month, 1).AddMonths(1);
+        private static DateTime firstInMonth2 = new DateTime(d.Year, d.Month, 1).AddMonths(2);
+
+        DateTime startDate;
+        DateTime endDate;
+        int customerId;
+        bool bookingResult;
+
+        public CreateBookingSteps()
+        {
+
+            fakeBookingRepository = new Mock<IRepository<Booking>>();
+
+            var bookings = new List<Booking>
+            {
+                new Booking
+                {
+                    Id = 1, CustomerId = 1, RoomId = 1, IsActive = true,
+                    StartDate = firstInMonth1,
+                    EndDate = firstInMonth2
+                },
+                new Booking
+                {
+                    Id = 2, CustomerId = 2, RoomId = 2, IsActive = true,
+                    StartDate = firstInMonth1,
+                    EndDate = firstInMonth2
+                }
+            };
+
+            // Implement fake GetAll() method.
+            fakeBookingRepository.Setup(x => x.GetAll()).Returns(bookings);
+
+            fakeBookingRepository.Setup(x => x.Add(It.IsAny<Booking>())).Returns(bookings[0]);
+
+
+            fakeRoomRepository = new Mock<IRepository<Room>>();
+
+            var rooms = new List<Room>
+            {
+                new Room { Id=1, Description="A" },
+                new Room { Id=2, Description="B" },
+            };
+
+            // Implement fake GetAll() method.
+            fakeRoomRepository.Setup(x => x.GetAll()).Returns(rooms);
+
+            bookingManager = new BookingManager(fakeBookingRepository.Object, fakeRoomRepository.Object);
+        }
+
+        [Given(@"I have entered a start date '(.*)'")]
+        public void GivenIHaveEnteredAStartDate(string _startDate)
+        {
+            startDate = DateTime.Parse(_startDate);
+        }
+        
+        [Given(@"I have entered a end date '(.*)'")]
+        public void GivenIHaveEnteredAEndDate(string _endDate)
+        {
+            endDate = DateTime.Parse(_endDate);
+        }
+        
+        [Given(@"I have entered a customer id (.*)")]
+        public void GivenIHaveEnteredACustomerId(int _customerId)
+        {
+            customerId = _customerId;
+        }
+        
+        [When(@"I press button Create booking")]
+        public void WhenIPressButtonCreateBooking()
+        {
+            var booking = new Booking
+            {
+                CustomerId = customerId,
+                StartDate = startDate,
+                EndDate = endDate
+            };
+
+            bookingResult = bookingManager.CreateBooking(booking);
+           
+        }
+        
+        [Then(@"The result should be (.*)")]
+        public void ThenTheResultShouldBe(string p0)
+        {
+            Assert.True(bookingResult); 
+        }
+    }
+}
---
+++ SpecflowTests/CreateBooking.feature
@@ -0,0 +1,18 @@
+﻿Feature: CreateBooking
+	In order to avoid silly mistakes
+	As a general idiot
+	I want to be able to create a booking
+
+@mytag
+Scenario Outline: Create booking
+	Given I have entered a start date <startDate>
+	And I have entered a end date <endDate>
+	And I have entered a customer id <CustomerId>
+	When I press button Create booking
+	Then The result should be <true>
+
+	Examples:
+	| id	|CustomerId		| startDate		| endDate		| 
+	| 47	| 39			|'2020-01-05'	|'2020-01-05'	|
+	| 79	| 102			|'2021-02-10'	|'2021-03-11'	|
+	| 112	| 1				|'2022-04-04'	|'2022-05-05'	|
